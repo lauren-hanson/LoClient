33d98bcc859aa82242c27aeeeadd85396bcd4f2c
Lauren Hanson <lohanson33@gmail.com>
2023-07-31 12:02:26 -0500

    first commit

---
+++ src/LoHanson.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './LoHanson.css';
+
+function LoHanson() {
+  return (
+    <div className="LoHanson">
+      
+    </div>
+  );
+}
+
+export default LoHanson;
